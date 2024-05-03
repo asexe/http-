53198a96f969c6fe89e08218d944518c0a35df64
mmdnmz <1344196791@qq.com>
2024-05-03 16:39:15 +0800

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -8,58 +8,79 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+// Function to parse the request and extract the path
+std::string extractPath(const std::string& request) {
+    std::string path;
+    size_t startPos = request.find("GET /");
+    if (startPos != std::string::npos) {
+        size_t endPos = request.find(" ", startPos + 5);
+        if (endPos != std::string::npos) {
+            path = request.substr(startPos + 5, endPos - startPos - 5);
+        }
+    }
+    return path;
+}
+
 int main(int argc, char **argv) {
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-std::cout << "Logs from your program will appear here!\n";
-std::string report;
+    std::cout << "Logs from your program will appear here!\n";
 
-// Uncomment this block to pass the first stage
-int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-if (server_fd < 0) {
-std::cerr << "Failed to create server socket\n";
-return 1;
-}
-//
-// // Since the tester restarts your program quite often, setting REUSE_PORT
-// // ensures that we don't run into 'Address already in use' errors
-int reuse = 1;
-if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-std::cerr << "setsockopt failed\n";
-return 1;
-}
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        std::cerr << "Failed to create server socket\n";
+        return 1;
+    }
 
-struct sockaddr_in server_addr;
-server_addr.sin_family = AF_INET;
-server_addr.sin_addr.s_addr = INADDR_ANY;
-server_addr.sin_port = htons(4221);
-//
-if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-std::cerr << "Failed to bind to port 4221\n";
-return 1;
-}
-//
-int connection_backlog = 5;
-if (listen(server_fd, connection_backlog) != 0) {
-std::cerr << "listen failed\n";
-return 1;
-}
-//
-struct sockaddr_in client_addr;
-int client_addr_len = sizeof(client_addr);
+    int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
+        std::cerr << "setsockopt failed\n";
+        return 1;
+    }
 
-std::cout << "Waiting for a client to connect...\n";
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(4221);
 
-int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-printf("%d\n",client_fd);
-if (client_fd < 0){
-  std::cout<<"Connect Failed"; 
-}
-else{
-  std::cout << "Client connected\n";
-  report = "HTTP/1.1 200 OK\r\n\r\n";
-  send(client_fd, report.c_str(), report.length(), 0);
-}
-close(server_fd);
+    if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+        std::cerr << "Failed to bind to port 4221\n";
+        return 1;
+    }
 
-  return 0;
+    int connection_backlog = 5;
+    if (listen(server_fd, connection_backlog) != 0) {
+        std::cerr << "listen failed\n";
+        return 1;
+    }
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    std::cout << "Waiting for a client to connect...\n";
+
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+    if (client_fd < 0) {
+        std::cout << "Connect Failed\n";
+    } else {
+        std::cout << "Client connected\n";
+
+        char buffer[1024];
+        ssize_t n = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+        if (n > 0) {
+            buffer[n] = '\0'; // Null-terminate the string
+            std::string request(buffer);
+
+            std::string path = extractPath(request);
+            if (path == "/index.html") {
+                std::string response = "HTTP/1.1 200 OK\r\n\r\n";
+                send(client_fd, response.c_str(), response.length(), 0);
+            } else {
+                std::string response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                send(client_fd, response.c_str(), response.length(), 0);
+            }
+        }
+    }
+
+    close(server_fd);
+
+    return 0;
 }
